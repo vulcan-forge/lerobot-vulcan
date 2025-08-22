9e71ca7a5eadacf11eb978a0235392dd9028bd74
Myles Reda <myles.reda2@gmail.com>
2025-08-22 10:59:28 -0400

    dc_motor_test update

--- start/myles/dc_motor_test.py
+++ start/myles/dc_motor_test.py
@@ -18,8 +18,8 @@ def test_motor_turn():
 
         # Motor config for testing
         motor_config = {
-            "pwm_pins": [23, 27],           # IN1 - PWM control for both motors
-            "direction_pins": [24, 22],      # IN2 - Direction control for both motors
+            "pwm_pins": [24, 27],           # IN1 - PWM control for both motors
+            "direction_pins": [23, 22],      # IN2 - Direction control for both motors
             "pwm_frequency": 1000,
             "invert_direction": False,
         }
