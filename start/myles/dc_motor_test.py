eb4c9455734d5a8be422828dba276cd4dfea2fa1
Myles Reda <myles.reda2@gmail.com>
2025-08-22 16:52:05 -0400

    dc_motor_test update

--- start/myles/dc_motor_test.py
+++ start/myles/dc_motor_test.py
@@ -74,7 +74,7 @@ def test_motor_turn():
             controller.set_velocity("rear_left", -1.0)
             controller.set_velocity("rear_right", 1.0)
             print("   Motor states: ", controller.protocol_handler.motor_states)
-            time.sleep(30)
+            time.sleep(3)
 
             # Stop motors
             print("3. Stopping motor...")
