006be48f91bca2b4c7ac4d4bb0f6627939a4724d
Myles Reda <myles.reda2@gmail.com>
2025-08-22 12:42:49 -0400

    dc_motor_test update

--- start/myles/dc_motor_test.py
+++ start/myles/dc_motor_test.py
@@ -63,7 +63,7 @@ def test_motor_turn():
         try:
             # Turn motor 1 and 2 forward at full speed for 5 seconds
             print("2. Turning forward for 5 seconds...")
-            controller.set_velocity("front_left", 1.0)
+            controller.set_velocity("front_right", 1.0)
             print("   Motor states: ", controller.protocol_handler.motor_states)
             time.sleep(5)
 
