98d25a33274b5851909b40f923659e095b403918
Myles Reda <myles.reda2@gmail.com>
2025-08-22 14:24:15 -0400

    dc_motor_test update

--- start/myles/dc_motor_test.py
+++ start/myles/dc_motor_test.py
@@ -69,10 +69,10 @@ def test_motor_turn():
         try:
             # Robot forward for 3 seconds
             print("2. Forward for 3 seconds...")
-            controller.set_velocity("front_left", -1.0)
-            controller.set_velocity("front_right", -1.0)
-            controller.set_velocity("rear_left", -1.0)
-            controller.set_velocity("rear_right", -1.0)
+            controller.set_velocity("front_left", 1.0)
+            controller.set_velocity("front_right", 1.0)
+            controller.set_velocity("rear_left", 1.0)
+            controller.set_velocity("rear_right", 1.0)
             print("   Motor states: ", controller.protocol_handler.motor_states)
             time.sleep(3)
 
