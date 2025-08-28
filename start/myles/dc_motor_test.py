2221de42e4f393b2418bb875d0c46160fedd5a77
Myles Reda <myles.reda2@gmail.com>
2025-08-28 15:33:07 -0400

    dc_motor_test update

--- start/myles/dc_motor_test.py
+++ start/myles/dc_motor_test.py
@@ -70,11 +70,11 @@ def test_motor_turn():
             #Wheel Test
             print("2. Wheel Test...")
             controller.set_velocity("linear_actuator", 1.0)
-            time.sleep(2)
+            time.sleep(5)
             controller.set_velocity("linear_actuator", 0.0)
             time.sleep(1)
             controller.set_velocity("linear_actuator", -1.0)
-            time.sleep(2)
+            time.sleep(5)
 
             # Stop motors
             print("3. Stopping motor...")
