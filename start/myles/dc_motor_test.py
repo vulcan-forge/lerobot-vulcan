d950a89c54be635eb9fadcee0fd0e739b816dc93
Myles Reda <myles.reda2@gmail.com>
2025-08-28 14:48:24 -0400

    dc_motor_test update

--- start/myles/dc_motor_test.py
+++ start/myles/dc_motor_test.py
@@ -18,8 +18,8 @@ def test_motor_turn():
 
         # Motor config for testing
         motor_config = {
-            "in1_pins": [22,17,15,4,2],
-            "in2_pins": [23,27,18,14,3],
+            "in1_pins": [17,22,24,16,20],
+            "in2_pins": [27,23,25,26,21],
             "pwm_frequency": 1000,
         }
 
