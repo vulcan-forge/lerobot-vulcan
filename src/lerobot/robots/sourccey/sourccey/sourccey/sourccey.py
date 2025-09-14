9378d72e61d24246ba290a0f573b81452075f1de
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-09-13 20:36:01 -0400

    camera cleanup and send action cleanup

--- src/lerobot/robots/sourccey/sourccey/sourccey/sourccey.py
+++ src/lerobot/robots/sourccey/sourccey/sourccey/sourccey.py
@@ -266,16 +266,6 @@ class Sourccey(Robot):
             self.dc_motors_controller.set_velocities(wheel_action)
 
             sent_action = {**prefixed_send_action_left, **prefixed_send_action_right, **base_goal_vel}
-            if random.random() < 0.02:
-                print()
-                print()
-                print()
-                print(f"Action received: {action}")
-                print(f"Action sent: {sent_action}")
-                print()
-                print()
-                print()
-
             return sent_action
         except Exception as e:
             print(f"Error sending action: {e}")
