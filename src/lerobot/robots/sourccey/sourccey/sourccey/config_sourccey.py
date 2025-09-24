697b6f366b467f8ec4f5017c4309ae99632951c2
CoMa <colinmm3@icloud.com>
2025-09-24 16:09:18 -0400

    camera rotation

--- src/lerobot/robots/sourccey/sourccey/sourccey/config_sourccey.py
+++ src/lerobot/robots/sourccey/sourccey/sourccey/config_sourccey.py
@@ -25,10 +25,10 @@ from lerobot.constants import HF_LEROBOT_CONFIGURATION
 def sourccey_cameras_config() -> dict[str, CameraConfig]:
     config = {
         "front_left": OpenCVCameraConfig(
-            index_or_path="/dev/cameraFrontLeft", fps=30, width=640, height=360, rotation=Cv2Rotation.ROTATE_180
+            index_or_path="/dev/cameraFrontLeft", fps=30, width=640, height=360
         ),
         "front_right": OpenCVCameraConfig(
-            index_or_path="/dev/cameraFrontRight", fps=30, width=640, height=360, rotation=Cv2Rotation.ROTATE_180
+            index_or_path="/dev/cameraFrontRight", fps=30, width=640, height=360
         ),
         "wrist_left": OpenCVCameraConfig(
             index_or_path="/dev/cameraWristLeft", fps=30, width=640, height=360
