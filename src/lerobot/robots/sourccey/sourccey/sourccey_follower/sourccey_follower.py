30e3a98f0e8e5f14ef887e35a037257d3af3c38a
Sourccey <sourccey@gmail.com>
2025-09-25 18:05:19 -0400

    PID-update

--- src/lerobot/robots/sourccey/sourccey/sourccey_follower/sourccey_follower.py
+++ src/lerobot/robots/sourccey/sourccey/sourccey_follower/sourccey_follower.py
@@ -117,7 +117,7 @@ class SourcceyFollower(Robot):
 
             if motor == "gripper":
                 # Set P_Coefficient to lower value to avoid shakiness (Default is 32)
-                self.bus.write("P_Coefficient", motor, 225)
+                self.bus.write("P_Coefficient", motor, 210)
                 # Set I_Coefficient and D_Coefficient to default value 0 and 32
                 self.bus.write("I_Coefficient", motor, 20)
                 self.bus.write("D_Coefficient", motor, 25)
