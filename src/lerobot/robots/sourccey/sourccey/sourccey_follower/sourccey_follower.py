6906eafb6da309079935df05fe039936d86015a6
Sourccey <sourccey@gmail.com>
2025-09-25 18:03:16 -0400

    PID-update

--- src/lerobot/robots/sourccey/sourccey/sourccey_follower/sourccey_follower.py
+++ src/lerobot/robots/sourccey/sourccey/sourccey_follower/sourccey_follower.py
@@ -119,7 +119,7 @@ class SourcceyFollower(Robot):
                 # Set P_Coefficient to lower value to avoid shakiness (Default is 32)
                 self.bus.write("P_Coefficient", motor, 250)
                 # Set I_Coefficient and D_Coefficient to default value 0 and 32
-                self.bus.write("I_Coefficient", motor, 0)
+                self.bus.write("I_Coefficient", motor, 10)
                 self.bus.write("D_Coefficient", motor, 32)
                 self.bus.write("Max_Torque_Limit", motor, 500)  # 50% of max torque to avoid burnout
                 self.bus.write("Protection_Current", motor, 400)  # 50% of max current to avoid burnout
