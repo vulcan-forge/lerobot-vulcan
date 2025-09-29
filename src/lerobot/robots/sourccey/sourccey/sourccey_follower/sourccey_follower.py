ab89f0c0d196dc76f889f89b8240143525e6c6fb
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-09-29 15:48:42 -0400

    pid control

--- src/lerobot/robots/sourccey/sourccey/sourccey_follower/sourccey_follower.py
+++ src/lerobot/robots/sourccey/sourccey/sourccey_follower/sourccey_follower.py
@@ -112,31 +112,25 @@ class SourcceyFollower(Robot):
 
     def configure(self) -> None:
         self.bus.disable_torque()
-        for motor in self.bus.motors:            
+        for motor in self.bus.motors:
             self.bus.write("Operating_Mode", motor, OperatingMode.POSITION.value)
 
             if motor == "gripper":
-                # Set P_Coefficient to lower value to avoid shakiness (Default is 32)
                 self.bus.write("P_Coefficient", motor, 170)
-                # Set I_Coefficient and D_Coefficient to default value 0 and 32
                 self.bus.write("I_Coefficient", motor, 0)
                 self.bus.write("D_Coefficient", motor, 55)
                 self.bus.write("Max_Torque_Limit", motor, 500)  # 50% of max torque to avoid burnout
                 self.bus.write("Protection_Current", motor, 400)  # 50% of max current to avoid burnout
                 self.bus.write("Overload_Torque", motor, 25)  # 25% torque when overloaded
             elif motor == "shoulder_lift":
-                # Set P_Coefficient to lower value to avoid shakiness (Default is 32)
                 self.bus.write("P_Coefficient", motor, 16)
-                # Set I_Coefficient and D_Coefficient to default value 0 and 32
                 self.bus.write("I_Coefficient", motor, 0)
                 self.bus.write("D_Coefficient", motor, 32)
                 self.bus.write("Max_Torque_Limit", motor, 2000)
                 self.bus.write("Protection_Current", motor, 2500)
                 self.bus.write("Overload_Torque", motor, 25)  # 25% torque when overloaded
             else:
-                # Set P_Coefficient to lower value to avoid shakiness (Default is 32)
                 self.bus.write("P_Coefficient", motor, 16)
-                # Set I_Coefficient and D_Coefficient to default value 0 and 32
                 self.bus.write("I_Coefficient", motor, 0)
                 self.bus.write("D_Coefficient", motor, 32)
                 self.bus.write("Max_Torque_Limit", motor, 1500)  # 80% of max torque
