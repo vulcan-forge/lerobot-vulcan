ed2c67ee7968cd14015941f39bf3e679c49a2d43
ChristopherMaselli <theoriginalchris1@gmail.com>
2025-10-17 18:06:41 -0400

    0.45 steps

--- src/lerobot/motors/dc_motors_controller.py
+++ src/lerobot/motors/dc_motors_controller.py
@@ -221,7 +221,7 @@ class BaseDCMotorsController(abc.ABC):
             self._step_velocity_state = {}
 
         now = time.time()
-        step_size = 0.5
+        step_size = 0.45
         step_interval = 1.0
 
         # Add a start_time to the state for 0.5s at 0.5 velocity
