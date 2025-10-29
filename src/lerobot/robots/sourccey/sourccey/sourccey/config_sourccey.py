e9ce5d64c62f5df439a0c264de7de6cb3387f7ab
ChristopherMaselli <theoriginalchris1@gmail.com>
2025-10-29 12:51:32 -0400

    Attempt 3 at camera config change

--- src/lerobot/robots/sourccey/sourccey/sourccey/config_sourccey.py
+++ src/lerobot/robots/sourccey/sourccey/sourccey/config_sourccey.py
@@ -31,10 +31,10 @@ def sourccey_cameras_config() -> dict[str, CameraConfig]:
              index_or_path="/dev/cameraFrontRight", fps=30, width=640, height=480
          ),
          "wrist_left": OpenCVCameraConfig(
-             index_or_path="/dev/cameraWristLeft", fps=30, width=320, height=240
+             index_or_path="/dev/cameraWristLeft", fps=30, width=640, height=480
          ),
          "wrist_right": OpenCVCameraConfig(
-             index_or_path="/dev/cameraWristRight", fps=30, width=320, height=240
+             index_or_path="/dev/cameraWristRight", fps=30, width=640, height=480
          ),
     }
     return config
