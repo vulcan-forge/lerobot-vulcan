1668a290c59b470fd07fb8c347e0a81733a5fdd3
CoMa <colinmm3@icloud.com>
2025-11-11 16:28:14 -0500

    higher D lower P and I

--- src/lerobot/robots/sourccey/sourccey/sourccey_follower/sourccey_follower.py
+++ src/lerobot/robots/sourccey/sourccey/sourccey_follower/sourccey_follower.py
@@ -155,16 +155,16 @@ class SourcceyFollower(Robot):
                 self.bus.write("Protection_Current", motor, 400)  # 50% of max current to avoid burnout
                 self.bus.write("Overload_Torque", motor, 25)  # 25% torque when overloaded
             elif motor == "shoulder_lift":
-                self.bus.write("P_Coefficient", motor, 24)
-                self.bus.write("I_Coefficient", motor, 2)
-                self.bus.write("D_Coefficient", motor, 16)
+                self.bus.write("P_Coefficient", motor, 16)
+                self.bus.write("I_Coefficient", motor, 0)
+                self.bus.write("D_Coefficient", motor, 48)  # Higher D for more damping
                 self.bus.write("Max_Torque_Limit", motor, 2000)
                 self.bus.write("Protection_Current", motor, 4200)  # 4.2A for STS3250
                 self.bus.write("Overload_Torque", motor, 25)  # 25% torque when overloaded
             elif motor == "elbow_flex":
-                self.bus.write("P_Coefficient", motor, 24)
-                self.bus.write("I_Coefficient", motor, 2)
-                self.bus.write("D_Coefficient", motor, 16)
+                self.bus.write("P_Coefficient", motor, 16)
+                self.bus.write("I_Coefficient", motor, 0)
+                self.bus.write("D_Coefficient", motor, 48)  # Higher D for more damping
                 self.bus.write("Max_Torque_Limit", motor, 2000)
                 self.bus.write("Protection_Current", motor, 4200)  # 4.2A for STS3250
                 self.bus.write("Overload_Torque", motor, 25)  # 25% torque when overloaded
