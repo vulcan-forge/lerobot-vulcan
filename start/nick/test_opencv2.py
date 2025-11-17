c4245f1dcfc96b1a931675dad18c23c0686fd8a1
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-11-16 23:21:28 -0500

    setup cleanup

--- start/nick/test_opencv2.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-cap = cv2.VideoCapture("tests/artifacts/cameras/image_160x120.png", cv2.CAP_FFMPEG)
-print("FFmpeg open:", cap.isOpened())
-cap.release()
-
-cap2 = cv2.VideoCapture("tests/artifacts/cameras/image_160x120.png", cv2.CAP_DSHOW)
-print("DirectShow open:", cap2.isOpened())
-cap2.release()
