be21e5511121fad827d94df84004ead72445bec1
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-12-12 14:42:26 -0500

    model skeleton

--- src/lerobot/policies/vulcan0/modeling_vulcan0.py
+++ src/lerobot/policies/vulcan0/modeling_vulcan0.py
@@ -1,3 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 from torch import Tensor
 from lerobot.policies.pretrained import PreTrainedPolicy
 from lerobot.policies.vulcan0.configuration_vulcan0 import Vulcan0Config
@@ -11,5 +15,16 @@ class Vulcan0Policy(PreTrainedPolicy):
         super().__init__(config)
         self.config = config
 
+        self.model = Vulcan0Model(config)
+
     def forward(self, batch: dict[str, Tensor]) -> tuple[Tensor, dict]:
         return self.model(batch)
+
+
+class Vulcan0Model(nn.Module):
+    def __init__(self, config: Vulcan0Config):
+        super().__init__()
+        self.config = config
+
+    def forward(self, batch: dict[str, Tensor]) -> tuple[Tensor, dict]:
+        pass
