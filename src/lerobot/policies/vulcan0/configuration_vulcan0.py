d5096021689e1f59e1ce4d3becfb0345e2e4f51d
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-12-17 11:17:41 -0500

    encoder decoder working

--- src/lerobot/policies/vulcan0/configuration_vulcan0.py
+++ src/lerobot/policies/vulcan0/configuration_vulcan0.py
@@ -8,3 +8,6 @@ from lerobot.configs.policies import PreTrainedConfig
 class Vulcan0Config(PreTrainedConfig):
 
     model_dimension: int = 512
+    num_heads: int = 8
+    feedforward_dim: int = 3200
+    dropout: float = 0.1
--- src/lerobot/policies/vulcan0/modeling_vulcan0.py
+++ src/lerobot/policies/vulcan0/modeling_vulcan0.py
@@ -5,47 +5,39 @@ from lerobot.policies.vulcan0.configuration_vulcan0 import Vulcan0Config
 
 class Vulcan0Model(nn.Module):
     def __init__(self, config: Vulcan0Config):
-        super().__init__()
-        self.config = config
+        pass
 
 class Vulcan0Encoder(nn.Module):
     def __init__(self, config: Vulcan0Config):
         super().__init__()
 
-        self.model_dimension = 512
-        self.num_heads = 8
-        self.dim_feedforward = 3200
-        self.dropout = 0.1
-
-        self.multi_head_self_attention = nn.MultiheadAttention(embed_dim=self.model_dimension, num_heads=self.num_heads, dropout=self.dropout)
+        self.multi_head_self_attention = nn.MultiheadAttention(embd_dim=config.model_dimension, num_heads=config.num_heads)
         self.multi_layer_perceptron = nn.Sequential(
-            nn.Linear(self.model_dimension, self.dim_feedforward),
+            nn.Linear(config.model_dimension, config.feedforward_dim),
             nn.GELU(),
-            nn.Linear(self.dim_feedforward, self.model_dimension),
-            nn.Dropout(self.dropout)
+            nn.Linear(config.feedforward_dim, config.model_dimension)
         )
 
-        self.layer_norm1 = nn.LayerNorm(self.model_dimension)
-        self.layer_norm2 = nn.LayerNorm(self.model_dimension)
-        self.dropout1 = nn.Dropout(self.dropout)
-        self.dropout2 = nn.Dropout(self.dropout)
+        self.layer_norm1 = nn.LayerNorm(config.model_dimension)
+        self.layer_norm2 = nn.LayerNorm(config.model_dimension)
+        self.dropout1 = nn.Dropout(config.dropout)
+        self.dropout2 = nn.Dropout(config.dropout)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-
         '''
-        Input: (S, B, D): S = Sequence Length, B = Batch Size, D = Model Dimension
-        Output: (S, B, D): S = Sequence Length, B = Batch Size, D = Model Dimension
+        Input: (S, B, D) -> S: Sequence length, B: Batch size, D: Feature dimension
+        Output: (S, B, D) -> S: Sequence length, B: Batch size, D: Feature dimension
         '''
 
-        # Pre norm so we normalize (mean = 0, std = 1) before the attention layer
+        # 1. Multi-head Self-Attention
         x_norm1 = self.layer_norm1(x)
-        attention_output, _ = self.multi_head_self_attention(x_norm1, x_norm1, x_norm1, need_weights=False) # q, k, v: all the same
-        x = x + self.dropout1(attention_output)
+        attention_output, _ = self.multi_head_self_attention(x_norm1, x_norm1, x_norm1, need_weights=False)
+        x = x + nn.Dropout(attention_output)
 
-        # After the attention layer we normalize again, then apply the MLP
+        # 2. Feed-Forward Layer
         x_norm2 = self.layer_norm2(x)
         mlp_output = self.multi_layer_perceptron(x_norm2)
-        x = x + self.dropout2(mlp_output)
+        x = x + nn.Dropout(mlp_output)
 
         return x
 
@@ -53,47 +45,40 @@ class Vulcan0Decoder(nn.Module):
     def __init__(self, config: Vulcan0Config):
         super().__init__()
 
-        self.model_dimension = 512
-        self.num_heads = 8
-        self.dim_feedforward = 3200
-        self.dropout = 0.1
-
-        self.multi_head_self_attention = nn.MultiheadAttention(embed_dim=self.model_dimension, num_heads=self.num_heads, dropout=self.dropout)
-        self.cross_attention = nn.MultiheadAttention(embed_dim=self.model_dimension, num_heads=self.num_heads, dropout=self.dropout)
+        self.multi_head_self_attention = nn.MultiheadAttention(embd_dim=config.model_dimension, num_heads=config.num_heads)
+        self.cross_attention = nn.MultiheadAttention(embd_dim=config.model_dimension, num_heads=config.num_heads)
         self.multi_layer_perceptron = nn.Sequential(
-            nn.Linear(self.model_dimension, self.dim_feedforward),
+            nn.Linear(config.model_dimension, config.feedforward_dim),
             nn.GELU(),
-            nn.Linear(self.dim_feedforward, self.model_dimension),
-            nn.Dropout(self.dropout)
+            nn.Linear(config.feedforward_dim, config.model_dimension)
         )
-
-        self.layer_norm1 = nn.LayerNorm(self.model_dimension)
-        self.layer_norm2 = nn.LayerNorm(self.model_dimension)
+        self.layer_norm1 = nn.LayerNorm(config.model_dimension)
+        self.layer_norm2 = nn.LayerNorm(config.model_dimension)
         self.layer_norm3 = nn.LayerNorm(self.model_dimension)
-        self.dropout1 = nn.Dropout(self.dropout)
-        self.dropout2 = nn.Dropout(self.dropout)
-        self.dropout3 = nn.Dropout(self.dropout)
+        self.dropout1 = nn.Dropout(config.dropout)
+        self.dropout2 = nn.Dropout(config.dropout)
+        self.dropout3 = nn.Dropout(config.dropout)
 
     def forward(self, x: torch.Tensor, encoder_out: torch.Tensor) -> torch.Tensor:
         '''
-        Input: (S, B, D): S = Sequence Length, B = Batch Size, D = Model Dimension
-        Encoder Output: (S, B, D): S = Sequence Length, B = Batch Size, D = Model Dimension
-        Output: (S, B, D): S = Sequence Length, B = Batch Size, D = Model Dimension
+        Input: (S, B, D) -> S: Sequence length, B: Batch size, D: Feature dimension
+        Encoder Output: (S, B, D) -> S: Sequence length, B: Batch size, D: Feature dimension
+        Output: (S, B, D) -> S: Sequence length, B: Batch size, D: Action dimension
         '''
 
-        # Pre norm so we normalize (mean = 0, std = 1) before the attention layer
+        # 1. Multi-head Self-Attention
         x_norm1 = self.layer_norm1(x)
-        attention_output, _ = self.multi_head_self_attention(x_norm1, x_norm1, x_norm1, need_weights=False) # q, k, v: all the same
-        x = x + self.dropout1(attention_output)
+        attention_output, _ = self.multi_head_self_attention(x_norm1, x_norm1, x_norm1, need_weights=False)
+        x = x + nn.Dropout(attention_output)
 
-        # After the attention layer we normalize again, then apply the cross attention
+        # 2. Cross-Attention
         x_norm2 = self.layer_norm2(x)
-        cross_attention_output, _ = self.cross_attention(x_norm2, encoder_out, encoder_out, need_weights=False) # q: decoder, k, v: encoder
-        x = x + self.dropout2(cross_attention_output)
+        cross_attention_output, _ = self.cross_attention(x_norm2, encoder_out, encoder_out, need_weights=False)
+        x = x + nn.Dropout(cross_attention_output)
 
-        # After the cross attention layer we normalize again, then apply the MLP
+        # 3. Feed-Forward Layer
         x_norm3 = self.layer_norm3(x)
         mlp_output = self.multi_layer_perceptron(x_norm3)
-        x = x + self.dropout3(mlp_output)
+        x = x + nn.Dropout(mlp_output)
 
         return x
