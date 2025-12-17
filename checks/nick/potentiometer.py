924e8b4b3747b708b99d42a7c0083498092232e2
Myles Reda <myles.reda2@gmail.com>
2025-12-17 17:09:48 -0500

    sample rate change

--- checks/nick/potentiometer.py
+++ checks/nick/potentiometer.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
                     }
                 )
             )
-            time.sleep(0.2)
+            time.sleep(0.02)
     except KeyboardInterrupt:
         pass
     except Exception:
