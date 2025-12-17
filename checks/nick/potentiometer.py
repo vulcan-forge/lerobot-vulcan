39328a3539b61a316483b569639667c3a27d0776
Myles Reda <myles.reda2@gmail.com>
2025-12-17 17:41:03 -0500

    sampling rate change

--- checks/nick/potentiometer.py
+++ checks/nick/potentiometer.py
@@ -16,7 +16,7 @@ ADC_CHANNEL = 1
 VREF = 3.30
 
 # Sampling / smoothing (copied style from your battery.py)
-AVERAGE_SAMPLES = 4
+AVERAGE_SAMPLES = 8
 FILTER_ALPHA = 1.0  # 0..1, lower = smoother
 _filtered_value: Optional[float] = None
 
