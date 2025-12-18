b83ec0297dee397c6d40eb5b6a3523a66e86158f
Myles Reda <myles.reda2@gmail.com>
2025-12-18 17:40:48 -0500

    filter tuning

--- checks/nick/potentiometer.py
+++ checks/nick/potentiometer.py
@@ -21,7 +21,7 @@ POT_MECHANICAL_MAX = 77
 
 # Sampling / smoothing (copied style from your battery.py)
 AVERAGE_SAMPLES = 8
-FILTER_ALPHA = 0.6  # 0..1, lower = smoother
+FILTER_ALPHA = 0.4  # 0..1, lower = smoother
 _filtered_value: Optional[float] = None
 
 
