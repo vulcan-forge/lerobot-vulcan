c760edfecfbfd8313348de1b2df25c0a1734ebfd
Nicholas Maselli <maselli.nicholas@gmail.com>
2026-07-05 15:49:11 -0400

    untorque to improve calibration stability

--- src/lerobot/robots/sourccey/sourccey/sourccey/modules/torque/untorque.py
+++ src/lerobot/robots/sourccey/sourccey/sourccey/modules/torque/untorque.py
@@ -1,11 +1,18 @@
 import argparse
 
-from ..config_sourccey import SourcceyConfig
-from ..sourccey import Sourccey
+from ...config_sourccey import SourcceyConfig
+from ...sourccey import Sourccey
+
+
+def _resolve_target_arms(args: argparse.Namespace) -> tuple[bool, bool]:
+    use_left = args.left or not args.right
+    use_right = args.right or not args.left
+    return use_left, use_right
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Toggle Sourccey arm torque.")
+    parser.add_argument("--id", default="sourccey", help="Robot id to use when constructing the Sourccey config.")
     parser.add_argument("--left", action="store_true", help="Only affect the left arm.")
     parser.add_argument("--right", action="store_true", help="Only affect the right arm.")
     parser.add_argument("--enable", action="store_true", help="Enable torque instead of disabling it.")
@@ -13,18 +20,24 @@ def main() -> None:
     parser.add_argument("--right-arm-port", default=None, help="Override right arm serial port.")
     args = parser.parse_args()
 
-    use_left = args.left or not args.right
-    use_right = args.right or not args.left
+    use_left, use_right = _resolve_target_arms(args)
 
-    config = SourcceyConfig(id="sourccey")
+    config = SourcceyConfig(id=args.id)
     if args.left_arm_port:
         config.left_arm_port = args.left_arm_port
     if args.right_arm_port:
         config.right_arm_port = args.right_arm_port
 
     robot = Sourccey(config)
-    robot.connect()
+    connected_buses = []
     try:
+        if use_left:
+            robot.left_arm.bus.connect()
+            connected_buses.append(robot.left_arm.bus)
+        if use_right:
+            robot.right_arm.bus.connect()
+            connected_buses.append(robot.right_arm.bus)
+
         if args.enable:
             if use_left:
                 robot.left_arm.bus.enable_torque()
@@ -38,7 +51,8 @@ def main() -> None:
                 robot.right_arm.bus.disable_torque()
             print("Disabled torque.")
     finally:
-        robot.disconnect()
+        for bus in reversed(connected_buses):
+            bus.disconnect(disable_torque=not args.enable)
 
 
 if __name__ == "__main__":
